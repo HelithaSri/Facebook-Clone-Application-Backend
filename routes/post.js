5eeb299e1236c9b9a7a91115055b6a180e7b9124
HelithaSri <helitha.pravin@gmail.com>
2022-08-24 11:57:55 +0530

    post update done

--- routes/post.js
+++ routes/post.js
@@ -22,4 +22,38 @@ router.get('/:id', async (req, res) => {
         res.send("Err : " + err)
     }
 })
+
+router.post('/', async (req, res) => {
+    const post = await new Post({
+        userId: req.body.userId,
+        date: req.body.date,
+        time: req.body.time,
+        title: req.body.title,
+        body: req.body.body,
+    })
+    try {
+        const response = await post.save();
+        res.json(response)
+    } catch (err) {
+        res.send('Err : ' + err)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        post.userId = req.body.userId
+        post.date = req.body.date
+        post.time = req.body.time
+        post.title = req.body.title
+        post.body = req.body.body
+
+        const response = await post.save()
+        res.json(response)
+
+    } catch (err) {
+        res.send('Err : ' + err)
+    }
+})
+
 module.exports = router
