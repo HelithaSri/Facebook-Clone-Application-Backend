fdc61a633f5a98658163817a8e83ae703de32450
HelithaSri <helitha.pravin@gmail.com>
2022-08-24 11:41:24 +0530

    get by id

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const users = require('./routes/users')
+const post = require('./routes/post')
 
 const app = express()
 const port = 4000
@@ -17,6 +18,7 @@ con.on('open', () => {
 
 app.use(express.json())
 app.use('/users', users)
+app.use('/post', post)
 
 app.listen(port, () => {
     console.log(`app starting on ${port}`);
